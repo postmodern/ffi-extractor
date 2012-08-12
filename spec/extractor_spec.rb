6695d2e322d5b508af3a1b2e3dc22cbb7546c054
Postmodern <postmodern.mod3@gmail.com>
2012-08-11 19:18:24 -0700

    Added specs for Extractor.extract and Extractor.extract_from.

--- spec/extractor_spec.rb
+++ spec/extractor_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 require 'ffi/extractor'
 
+require 'yaml'
+
 describe FFI::Extractor do
   it "should have a VERSION constant" do
     subject.const_get('VERSION').should_not be_empty
@@ -10,14 +12,34 @@ describe FFI::Extractor do
     it "should throw :return" do
       lambda { subject.abort! }.should throw_symbol :return
     end
+  end
 
-    it "should stop the extraction process" do
+  let(:file) { 'spec/files/image.jpg'   }
+  let(:data) { File.new(file,'rb').read }
+
+  let(:metadata) { YAML.load_file('spec/files/image.meta') }
+
+  describe "extract" do
+    it "should extract metadata from a String" do
+      findings = []
+
+      subject.extract(data) do |*arguments|
+        findings << arguments
+      end
+
+      findings.should =~ metadata
     end
   end
 
-  describe "extract" do
-  end
-
   describe "extract_from" do
+    it "should extract metadata from a file" do
+      findings = []
+
+      subject.extract_from(file) do |*arguments|
+        findings << arguments
+      end
+
+      findings.should =~ metadata
+    end
   end
 end
